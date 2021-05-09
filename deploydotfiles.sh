f92e1b16830d64b9454ca4506ca83a54e165dbfc
epicbooperia <77999320+0xBooper@users.noreply.github.com>
2021-05-09 15:02:41 -0500

    Add some more things

--- deploydotfiles.sh
+++ deploydotfiles.sh
@@ -5,16 +5,21 @@
 # Proceed function
 proceed () {
   cd ~
+  
   echo "Updating system..."
-  sudo pacman -Syu
-  echo "Downloading required things..."
+  sudo pacman -Syu --noconfirm
+  
+  echo "Installing required things..."
   sudo pacman -S awesome git neofetch neovim base-devel xorg xorg-xinit zsh --noconfirm
+  
   echo "Getting dotfiles..."
   git clone https://github.com/0xBooper/dotfiles.git
+  
   echo "Deploying dotfiles..."
   cd dotfiles
   rm -rf .git README.md LICENSE .gitignore
   mv -f * ~
+  
   echo "Installing some other things..."
   cd ~
   git clone https://github.com/dylanaraps/pfetch.git
@@ -22,6 +27,18 @@ proceed () {
   sudo make install
   cd ~
   rm -rf pfetch
+  
+  echo "Downloading wallpapers..."
+  git clone https://github.com/makccr/wallpapers.git
+  
+  echo "Deploying wallpapers..."
+  cd wallpapers
+  cd 2020
+  mkdir ~/Wallpapers
+  mv *.jpg ~/Wallpapers
+  cd ~
+  rm -rf wallpapers
+  
   echo "Dotfiles have been fully deployed!"
   echo "Enjoy your system!"
   exit 0
