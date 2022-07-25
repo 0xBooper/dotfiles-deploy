fe3b7e82d153a3828e9974b6b0815c4879f72de2
0xBooper <yourhighboopness@gmail.com>
2022-07-24 21:45:56 -0500

    update script

--- deploydotfiles.sh
+++ deploydotfiles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # 0xBooper's dotfile deployer script. Licensed under the GPLv3 license.
 
 
@@ -13,7 +13,7 @@ proceed () {
   sudo pacman -S awesome git neofetch neovim base-devel xorg xorg-xinit zsh --noconfirm
   
   echo "Getting dotfiles..."
-  git clone https://github.com/0xBooper/dotfiles.git
+  git clone https://github.com/0xBooper/dotfiles.git 
   
   echo "Deploying dotfiles..."
   cd dotfiles
@@ -21,24 +21,22 @@ proceed () {
   mv -f * ~
   
   echo "Installing some other things..."
-  cd ~
+  echo "Downloading pfetch..."
   git clone https://github.com/dylanaraps/pfetch.git
-  cd pfetch
-  sudo make install
-  cd ~
-  rm -rf pfetch
+  echo "Installing pfetch..."
+  cd ~/pfetch && sudo make install
+  echo "Cleaning up..."
+  rm -rf ~/pfetch
   
   echo "Downloading wallpapers..."
   git clone https://github.com/makccr/wallpapers.git
   
   echo "Deploying wallpapers..."
-  cd wallpapers
-  cd 2020
-  mkdir ~/Wallpapers
-  mv *.jpg ~/Wallpapers
-  cd ~
-  rm -rf wallpapers
+  mkdir --parents ~/Media/Wallpapers
+  mv ~/wallpapers/wallpapers/*.jpg ~/Media/Wallpapers
+  rm -rf ~/wallpapers
   
+  bash
   echo "Dotfiles have been fully deployed!"
   echo "Enjoy your system!"
   exit 0
